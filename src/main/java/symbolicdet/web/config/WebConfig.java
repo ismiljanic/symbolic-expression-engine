5b32560e2c1b003fa5f425f37667cbf6eeb001a5
ismiljanic <ssmiljanicivan@gmail.com>
2026-02-27 11:17:38 +0100

    Add initial web config for CORS

---
+++ src/main/java/symbolicdet/web/config/WebConfig.java
@@ -0,0 +1,18 @@
+package symbolicdet.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*") // doesnt need to be too strict for our use case
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
